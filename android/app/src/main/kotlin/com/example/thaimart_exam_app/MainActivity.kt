3c43a860ff81ef92bfda192a27477af072b10c0d
Joey Wasin <wasin.ww1402@gmail.com>
2026-08-14 15:05:42 +0700

    build: init project

---
+++ android/app/src/main/kotlin/com/example/thaimart_exam_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.thaimart_exam_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/src/widgets/bases/paletteTheme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primarySwatch = MaterialColor(0xffdf3535, <int, Color>{
+  50: Color(0xfffceded),
+  100: Color(0xfff9dcdc),
+  200: Color(0xfff5b7b7),
+  300: Color(0xfff19090),
+  400: Color(0xffee6363),
+  500: Color(0xffdf3535),
+  600: Color(0xffb32828),
+  700: Color(0xff891d1d),
+  800: Color(0xff611111),
+  900: Color(0xff3c0707),
+  950: Color(0xff290404),
+});
+
+final Color primaryColor = primarySwatch.shade500;
