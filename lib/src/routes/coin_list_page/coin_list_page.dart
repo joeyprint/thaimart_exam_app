1dd3fb4345af5cc0620de1e540cb0bd617f466a4
Joey Wasin <wasin.ww1402@gmail.com>
2026-08-15 10:43:39 +0700

    feat: mock data of coin list

--- lib/src/routes/coin_list_page/coin_list_page.dart
+++ lib/src/routes/coin_list_page/coin_list_page.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:thaimart_exam_app/src/widgets/coin_card/coin_card.dart';
+import 'package:thaimart_exam_app/src/widgets/coin_list_tile/coin_list_tile.dart';
 
 class CoinListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Coins')),
-      body: Column(children: [CoinCard()]),
+      body: ListView(
+        children: [
+          CoinListTile(title: 'Bitcoin'),
+          CoinListTile(title: 'Coin'),
+          CoinListTile(title: 'XyzCoin'),
+          CoinListTile(title: 'Bigcoin'),
+          CoinListTile(title: 'Maxcoin'),
+          CoinListTile(title: 'Yellowcoin'),
+          CoinListTile(title: 'Dogecoin'),
+          CoinListTile(title: 'Bitcoin'),
+          CoinListTile(title: 'Coin'),
+          CoinListTile(title: 'XyzCoin'),
+          CoinListTile(title: 'Bigcoin'),
+          CoinListTile(title: 'Maxcoin'),
+          CoinListTile(title: 'Yellowcoin'),
+          CoinListTile(title: 'Dogecoin'),
+        ],
+      ),
     );
   }
 }
