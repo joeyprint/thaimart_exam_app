6a8d17b7f51bda0be0ade2292fb872ec1950be45
Joey Wasin <wasin.ww1402@gmail.com>
2026-08-15 11:48:03 +0700

    feat: add mock data of proce

--- lib/routes/coin_details_page/coin_details_page.dart
+++ lib/routes/coin_details_page/coin_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:thaimart_exam_app/widgets/bases/palette_theme.dart';
+import 'package:thaimart_exam_app/widgets/bases/typography_theme.dart';
 
 class CoinDetailsPage extends StatelessWidget {
   const CoinDetailsPage({super.key});
@@ -9,7 +10,30 @@ class CoinDetailsPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Bitcoin')),
       body: Container(
         color: primarySwatch.shade50,
-        child: Center(child: Text('COIN DETAILS')),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Last price"),
+                    Text('\$12345', style: textTheme.headlineLarge),
+                    Row(
+                      spacing: 8,
+                      children: [
+                        Text('+5.5%', style: TextStyle(color: Colors.green)),
+                        Text('Last 24 Hours'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
