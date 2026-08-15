7d046bf0780700a93eb45f58793d370243433bca
Joey Wasin <wasin.ww1402@gmail.com>
2026-08-15 10:42:41 +0700

    feat: update name of coin card to list tile

---
+++ lib/src/widgets/coin_list_tile/coin_list_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:thaimart_exam_app/src/widgets/bases/palette_theme.dart';
+import 'package:thaimart_exam_app/src/widgets/bases/typography_theme.dart';
+
+class CoinListTile extends StatelessWidget {
+  final String title;
+  final GestureTapCallback? onTap;
+
+  const CoinListTile({super.key, required this.title, this.onTap});
+
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        color: primarySwatch.shade100,
+        child: AspectRatio(
+          aspectRatio: 1 / 1,
+          child: Center(child: Text('Coin Icon')),
+        ),
+      ),
+      title: Text(title, style: TextStyle(fontWeight: FontWeight(600))),
+      subtitle: const Text('Description'),
+      trailing: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text('\$12345', style: textTheme.bodyLarge),
+          Text(
+            '+5.5%',
+            style: textTheme.bodySmall?.merge(TextStyle(color: Colors.green)),
+          ),
+        ],
+      ),
+      onTap: onTap,
+    );
+  }
+}
